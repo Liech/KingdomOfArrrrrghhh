02e82e61e6a6ed4ce599f968e96f80027cd95fcd
Liech <nicolas_harttig@hotmail.de>
2022-07-15 23:15:46 +0200

    dice rolling result

--- Project/Assets/Objects/Dice/DiceResult.cs
+++ Project/Assets/Objects/Dice/DiceResult.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class DiceResult : MonoBehaviour
-{
+public class DiceResult : MonoBehaviour {
     public List<Transform> DicePlanes;
-    
+
+
+    Rigidbody body;
+    bool currentlyRolling = false;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (body.velocity.magnitude < 1e-4 && currentlyRolling) {
+            Debug.Log("Rolling Dice Result: " + (getBestPlane() + 1).ToString());
+            currentlyRolling = false;
+        }
+        if (body.velocity.magnitude > 1)
+            currentlyRolling = true;
+    }
+
+    int getBestPlane() {
+        var myDir = new Vector3(0.0f, 1.0f, 0.0f);
+        float bestMatchValue = float.PositiveInfinity;
+        int bestIndex = 0;
+        for (int i = 0; i < DicePlanes.Count; i++) {
+            var planeDir = DicePlanes[i].rotation * -new Vector3(0.0f, 1.0f, 0.0f);
+            float matchValue = Vector3.Dot(myDir, planeDir);
+            if (matchValue < bestMatchValue) {
+                bestIndex = i;
+                bestMatchValue = matchValue;
+            }
+        }
+
+        return bestIndex;
     }
 }
