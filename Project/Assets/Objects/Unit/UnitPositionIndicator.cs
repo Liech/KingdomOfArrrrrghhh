cde0af7b18e9d21120b59232f37943b714e1b51f
Liech <nicolas_harttig@hotmail.de>
2022-07-16 02:20:01 +0200

    fixed unit position indicator

--- Project/Assets/Objects/Unit/UnitPositionIndicator.cs
+++ Project/Assets/Objects/Unit/UnitPositionIndicator.cs
@@ -7,6 +7,9 @@ public class UnitPositionIndicator : MonoBehaviour
 {
     HexBoard board;
 
+    Vector3Int oldHighlight;
+    bool hasOldHighlight = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +19,14 @@ public class UnitPositionIndicator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (hasOldHighlight)
+            board.highlightTilemap.SetTile(oldHighlight, null);
+
         Vector3Int cellID = board.GetComponent<Grid>().WorldToCell(transform.position);
-        //cellID.z = 0;
+        cellID.z = 0;
         var cell = board.highlightTilemap.GetTile(cellID);        
         board.highlightTilemap.SetTile(cellID, board.highlightTile);
+        hasOldHighlight = true;
+        oldHighlight = cellID;
     }
 }
