b8482d622cf6d94ddd17fcdd28be43924da9c458
Liech <nicolas_harttig@hotmail.de>
2022-07-16 21:16:23 +0200

    color indication

--- Project/Assets/Objects/Unit/FactionColorIndicator.cs
+++ Project/Assets/Objects/Unit/FactionColorIndicator.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(FactionMember))]
-public class FactionColorIndicator : MonoBehaviour
-{
+public class FactionColorIndicator : MonoBehaviour {
     public MeshRenderer colorTarget = null;
-    void Start()
-    {
-        if (!colorTarget)
-            return;
-        colorTarget.material.color = GetComponent<FactionMember>().getFaction().Color;
+    public SkinnedMeshRenderer colorTarget2 = null;
+    void Start() {
+        if (colorTarget)
+            colorTarget.material.color = GetComponent<FactionMember>().getFaction().Color;
+        if (colorTarget2)
+            colorTarget2.material.color = GetComponent<FactionMember>().getFaction().Color;
     }
 
     // Update is called once per frame
