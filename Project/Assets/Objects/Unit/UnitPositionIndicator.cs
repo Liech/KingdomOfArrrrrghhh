54701c68dca6d7cad775692c637799547be01e04
Liech <nicolas_harttig@hotmail.de>
2022-07-17 20:29:51 +0200

    removed buggy position indicator

--- Project/Assets/Objects/Unit/UnitPositionIndicator.cs
+++ Project/Assets/Objects/Unit/UnitPositionIndicator.cs
@@ -19,14 +19,14 @@ public class UnitPositionIndicator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (hasOldHighlight)
-            board.highlightTilemap.SetTile(oldHighlight, null);
-
-        Vector3Int cellID = board.GetComponent<Grid>().WorldToCell(transform.position);
-        cellID.z = 0;
-        var cell = board.highlightTilemap.GetTile(cellID);        
-        board.highlightTilemap.SetTile(cellID, board.highlightTile);
-        hasOldHighlight = true;
-        oldHighlight = cellID;
+        //if (hasOldHighlight)
+        //    board.highlightTilemap.SetTile(oldHighlight, null);
+        //
+        //Vector3Int cellID = board.GetComponent<Grid>().WorldToCell(transform.position);
+        //cellID.z = 0;
+        //var cell = board.highlightTilemap.GetTile(cellID);        
+        //board.highlightTilemap.SetTile(cellID, board.highlightTile);
+        //hasOldHighlight = true;
+        //oldHighlight = cellID;
     }
 }
